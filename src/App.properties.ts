b0e3dfcfe58bee540f570615247976675c5d82ac
kizuna-system <kizuna_system@kizunamaker.jp>
2021-08-19 09:36:57 +0900

    2

--- src/App.properties.ts
+++ src/App.properties.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://localhost:5000';
-export const BASE_URL = 'http://192.168.1.80:5002';
+export const BASE_URL = 'http://localhost:5000';
+
 
 export const BASE_PATH = '/itmanagement';
 export const BASE_API_PATH:string = BASE_URL + '/api/itmanagement'; 
